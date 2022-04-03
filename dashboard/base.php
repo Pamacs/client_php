95abb61ea9f6794639ea841b9ffb5d8e84809e24
ItzKernel <therealkernelpanic@gmail.com>
2022-04-03 20:39:14 +0200

    sidebar and most importantly astolfo

--- dashboard/base.php
+++ dashboard/base.php
@@ -19,12 +19,18 @@ function generate_head(string $title, array $stylesheets = []) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 '.$sheetstr.
 '    <title>'.$title.'</title>
 </head>
 ';
 
     echo $dom;
+}
+
+function create_sidebar() {
+    echo
+'';
 }
 
 function end_dom() {
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,7 +1,20 @@
 <?php require_once 'base.php'; generate_head("Pamacs", ['dashboard']); ?>
 <body>
-    <div>
-        <span>hii~ :3</span>
-    </div>
+    <sidebar>
+        <!-- Astolfo is a placeholder until we have a gud icon -->
+        <div id="sidebar:sign">
+            <img src="/image/astolfo.png" id="sign:icon">
+            <span id="sign:name">Pamacs</span>
+        </div>
+        <hr>
+        <a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span>Containers</a>
+        <a href="" class="sidebar-btn"><span class="material-icons">person</span>User</a>
+        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">local_fire_department</span>Src</a>
+
+        <a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span>Logout</a>
+    </sidebar>
+    <main>
+        
+    </main>
 </body>
 <?php end_dom(); ?>
