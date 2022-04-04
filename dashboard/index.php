017302f014b8ba7ec3787b0a8713632bf79bbfc5
ItzKernel <therealkernelpanic@gmail.com>
2022-04-04 16:34:46 +0200

    responsive sidebar for dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,17 +1,17 @@
 <?php require_once 'base.php'; generate_head("Pamacs", ['dashboard']); ?>
 <body>
     <sidebar>
-        <!-- Astolfo is a placeholder until we have a gud icon -->
+        <!-- Astolfo is a placeholder until we have an icon -->
         <div id="sidebar:sign">
             <img src="/image/astolfo.png" id="sign:icon">
             <span id="sign:name">Pamacs</span>
         </div>
         <hr>
-        <a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span>Containers</a>
-        <a href="" class="sidebar-btn"><span class="material-icons">person</span>User</a>
-        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">local_fire_department</span>Src</a>
+        <a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span><span>Containers</span></a>
+        <a href="" class="sidebar-btn"><span class="material-icons">person</span><span>User</span></a>
+        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">local_fire_department</span><span>Src</span></a>
 
-        <a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span>Logout</a>
+        <a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span><span>Logout</span></a>
     </sidebar>
     <main>
         
