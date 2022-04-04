eb466246c1e0802262363b7140811880af26d040
ItzKernel <therealkernelpanic@gmail.com>
2022-04-04 18:07:48 +0200

    made sidebar generatable (cleaner html)

--- dashboard/base.php
+++ dashboard/base.php
@@ -28,9 +28,21 @@ function generate_head(string $title, array $stylesheets = []) {
     echo $dom;
 }
 
-function create_sidebar() {
+function generate_sidebar() {
     echo
-'';
+'<sidebar>
+<!-- Astolfo is a placeholder until we have an icon -->
+<div id="sidebar:sign">
+    <img src="/image/astolfo.png" id="sign:icon">
+    <span id="sign:name">Pamacs</span>
+</div>
+<hr/>
+<a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span><span>Containers</span></a>
+<a href="" class="sidebar-btn"><span class="material-icons">person</span><span>User</span></a>
+<a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">code</span><span>Src</span></a>
+
+<a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span><span>Logout</span></a>
+</sidebar>';
 }
 
 function end_dom() {
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,18 +1,6 @@
 <?php require_once 'base.php'; generate_head("Pamacs", ['dashboard']); ?>
 <body>
-    <sidebar>
-        <!-- Astolfo is a placeholder until we have an icon -->
-        <div id="sidebar:sign">
-            <img src="/image/astolfo.png" id="sign:icon">
-            <span id="sign:name">Pamacs</span>
-        </div>
-        <hr/>
-        <a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span><span>Containers</span></a>
-        <a href="" class="sidebar-btn"><span class="material-icons">person</span><span>User</span></a>
-        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">code</span><span>Src</span></a>
-
-        <a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span><span>Logout</span></a>
-    </sidebar>
+    <?php generate_sidebar(); ?>
     <main>
         <span id="header">Containers</span>
         <div id="containers">
