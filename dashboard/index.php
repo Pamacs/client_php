6cbd23803b4a0179bb00539b06f98e38decf8ee0
ItzKernel <therealkernelpanic@gmail.com>
2022-04-04 18:04:48 +0200

    fixed overflow, added header and changed src icon

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,15 +6,18 @@
             <img src="/image/astolfo.png" id="sign:icon">
             <span id="sign:name">Pamacs</span>
         </div>
-        <hr>
+        <hr/>
         <a href="" class="sidebar-btn"><span class="material-icons">space_dashboard</span><span>Containers</span></a>
         <a href="" class="sidebar-btn"><span class="material-icons">person</span><span>User</span></a>
-        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">local_fire_department</span><span>Src</span></a>
+        <a href="https://github.com/Pamacs/" class="sidebar-btn"><span class="material-icons">code</span><span>Src</span></a>
 
         <a href="" class="sidebar-btn logout-btn"><span class="material-icons">logout</span><span>Logout</span></a>
     </sidebar>
     <main>
-        
+        <span id="header">Containers</span>
+        <div id="containers">
+            
+        </div>
     </main>
 </body>
 <?php end_dom(); ?>
