8fb8c454c311f0368bba6d21fbcee3bf9b298323
ItzKernel <therealkernelpanic@gmail.com>
2022-04-07 21:42:19 +0200

    started containers page and kao util because idk

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,7 +4,18 @@
     <main>
         <span id="header">Containers</span>
         <div id="containers">
-            
+            <?php
+
+            session_start();
+            // I'll implement accesstoken validation later
+            require_once '../util/req_util.php';
+
+            $res = sendReq("containers/get_containers", ReqMethod::GET, true, [], $_SESSION['access_token']);
+
+            if (count($res['data']['containers']) == 0) {
+                echo '<img src="/image/no_containers.png"/>';
+            }
+            ?>
         </div>
     </main>
 </body>
