e1c37d5f059a4c34cdf528a67beb3d2cf1031b54
ItzKernel <therealkernelpanic@gmail.com>
2022-04-24 00:12:41 +0200

    updated kaoutil to use an enum

--- util/kao_util.php
+++ util/kao_util.php
@@ -1,7 +1,16 @@
 <?php
-class KaomojiList {
+
+enum KaoUtil {
+    case POSITIVE; case NEGATIVE;
     // sauce: http://kaomoji.ru/en/
-    public $POS = ['(o^▽^o)', '＼(￣▽￣)／', '(⁀ᗢ⁀)', 'o(≧▽≦)o', 'o(>ω<)o', '(≧◡≦)', '(๑˃ᴗ˂)ﻭ'];
-    public $NEG = ['(╥﹏╥)', '(ﾉω･､)', '(＿ ＿*) Z z z', '(￣ ￣|||)', '( ; ω ; )', '( ╥ω╥ )'];
+
+    public function getRandomKaomoji() {
+        return match($this) {
+            KaoType::POSITIVE => array_rand(['(o^▽^o)', '＼(￣▽￣)／', '(⁀ᗢ⁀)', 'o(≧▽≦)o', 'o(>ω<)o', '(≧◡≦)', '(๑˃ᴗ˂)ﻭ']),
+            KaoType::NEGATIVE => array_rand(['(╥﹏╥)', '(ﾉω･､)', '(＿ ＿*) Z z z', '(￣ ￣|||)', '( ; ω ; )', '( ╥ω╥ )']),
+        };
+    }
 }
+
+
 ?>
